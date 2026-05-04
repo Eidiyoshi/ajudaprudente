8c88a2a7ce0e78b11bb918986d6525a691f3da29
Kampuz <miguel.moret@unesp.br>
2026-05-04 01:12:40 -0300

    added db integration

--- app/lib/Evento.ts
+++ app/lib/Evento.ts
@@ -1,24 +1,26 @@
+import { prisma } from '@/lib/prisma'
 import { Status } from '@/app/lib/Status'
 
 export class Evento {
-    nome : string;
-    descricao : string;
-    data : string;
-    horarioInicio : string;
-    horarioFim : string;
-    local : string;
-    vagasDisponiveis : number;
-    status : Status;
+    public idEvento?: number;
+    private readonly nome : string;
+    private readonly descricao?: string;
+    private readonly data : string;
+    private readonly horarioInicio : string;
+    private readonly horarioFim : string;
+    private readonly local : string;
+    private vagasDisponiveis : number;
+    private status : Status;
 
     constructor(
         nome : string,
-        descricao : string,
         data : string,
         horarioInicio : string,
         horarioFim : string, 
         local : string,
         vagasDisponiveis : number,
-        status : Status
+        status : Status,
+        descricao?: string
     ) {
         this.nome = nome;
         this.descricao = descricao;
@@ -28,5 +30,30 @@ export class Evento {
         this.local = local;
         this.vagasDisponiveis = vagasDisponiveis;
         this.status = status;
+    }
+    
+    public async storeOnDb() {
+        if (this.idEvento === undefined) {
+            const lastEvento = await prisma.evento.findFirst({
+                select: { evento: true },
+                orderBy: { evento: "desc" },
+            });
+            this.idEvento = (lastEvento?.evento ?? 0) + 1;
+        }
+        const created = await prisma.evento.create({
+            data: {
+                nome: this.nome,
+                descricao: this.descricao,
+                data: this.data,
+                horarioInicio: this.horarioInicio,
+                horarioFim: this.horarioFim,
+                local: this.local,
+                vagasDisponiveis: this.vagasDisponiveis,
+                status: this.status
+            },
+        });
+
+        this.idEvento = created.evento;
+        return created;
     }
 }
