e0c9c1ec9176d2e9a1452ab9c0ef4516b6bcfa99
Kampuz <miguel.moret@unesp.br>
2026-05-30 19:39:45 -0300

    feat: include mockup in main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Link from 'next/link';
 
+import { NavBar } from "@/app/components/navBar";
+
 export default function Home() {
   return (
-    
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <NavBar />
       <Link href="/pages/organizador/registrar_evento">Criar novo evento</Link>
       <Link href="/perfil">Perfil</Link>
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
