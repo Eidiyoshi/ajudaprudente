ad1b6d9e2c49b6093e1807da479771b8e0f6a784
Kampuz <miguel.moret@unesp.br>
2026-06-06 11:41:34 -0300

    remove redundant method

--- app/api/eventos/route.ts
+++ app/api/eventos/route.ts
@@ -20,25 +20,3 @@ export async function GET() {
     );
   }
 }
-
-export async function POST(request: Request) {
-  try {
-    const body = await request.json();
-    const evento = await prisma.evento.create({
-      data: {
-        nome: body.nome,
-        descricao: body.descricao,
-        data: new Date(body.data),
-        horarioInicio: body.horarioInicio,
-        horarioFim: body.horarioFim,
-        local: body.local,
-        vagas: body.status ?? Status.Rascunho,
-        organizador: body.organizador,
-      },
-    });
-    return NextResponse.json(evento, { status: 201 });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error: "Erro ao criar evento"}, { status: 500 });
-  }
-}
