268661a497035490e7d29a303a52d5eb26098175
Kampuz <miguel.moret@unesp.br>
2026-06-06 11:33:55 -0300

    added post method

--- app/api/eventos/route.ts
+++ app/api/eventos/route.ts
@@ -1,5 +1,6 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
+import { Status } from "@/generated/prisma/enums"
 
 export async function GET() {
   try {
@@ -17,5 +18,27 @@ export async function GET() {
       { error: "Erro ao buscar eventos" },
       { status: 500 }
     );
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const evento = await prisma.evento.create({
+      data: {
+        nome: body.nome,
+        descricao: body.descricao,
+        data: new Date(body.data),
+        horarioInicio: body.horarioInicio,
+        horarioFim: body.horarioFim,
+        local: body.local,
+        vagas: body.status ?? Status.Rascunho,
+        organizador: body.organizador,
+      },
+    });
+    return NextResponse.json(evento, { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Erro ao criar evento"}, { status: 500 });
   }
 }
